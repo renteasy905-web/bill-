c4df6f301e38c226547a0cd4ecc6c9b7b1f56475
renteasy905-web <renteasy905@gmail.com>
2026-01-08 04:48:55 +0530

    Update api.js

--- Frontend/src/utils/api.js
+++ Frontend/src/utils/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://bill-inventory-backend.onrender.com/api", // ← Fixed: added /api
-  timeout: 10000,
+  baseURL: "https://bill-inventory-backend.onrender.com/api",
+  timeout: 60000,  // ← Change from 10000 to 60000 (60 seconds)
   headers: {
     "Content-Type": "application/json",
   },
