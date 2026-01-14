76b24a4c8c114cf4b104805a8c5b975e73d87a33
renteasy905-web <renteasy905@gmail.com>
2026-01-15 01:23:40 +0530

    Create productController.js

---
+++ Backend/controllers/productController.js
@@ -0,0 +1,34 @@
+const Product = require('../models/Product');
+
+exports.createProduct = async (req, res) => {
+  try {
+    const product = await Product.create(req.body);
+    res.status(201).json({ success: true, product });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
+
+exports.getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find().sort({ itemName: 1 });
+    res.status(200).json({ success: true, products });
+  } catch (error) {
+    res.status(500).json({ success: false, message: 'Server error' });
+  }
+};
+
+exports.updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    if (!product) {
+      return res.status(404).json({ success: false, message: 'Product not found' });
+    }
+    res.status(200).json({ success: true, product });
+  } catch (error) {
+    res.status(400).json({ success: false, message: error.message });
+  }
+};
